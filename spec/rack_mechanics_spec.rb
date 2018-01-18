abb8ebe4e57a526ab402d402f035387c13ee2812
David Kuruvilla <dapawn@gmail.com>
2018-01-18 22:04:09 +0000

    automatically backed up by learn

--- spec/rack_mechanics_spec.rb
+++ spec/rack_mechanics_spec.rb
@@ -3,7 +3,7 @@ require_relative './spec_helper'
 describe "App" do
 
   it 'wishes user a good morning in the morning' do
-    allow(Time).to receive(:now).and_return("2018-01-18 22:01:41 +0000")
+    allow(Time).to receive(:now).and_return("2018-01-18 10:01:41 +0000")
   #  allow(Time).to receive(:now){
   #    Time.new(2015,11,27,9,30)
   #  }
