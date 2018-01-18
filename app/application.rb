2ab25abd90c4147a6ff2c7b963914800b5cfda30
David Kuruvilla <dapawn@gmail.com>
2018-01-18 21:56:39 +0000

    automatically backed up by learn

---
+++ app/application.rb
@@ -0,0 +1,16 @@
+class Application
+
+  def call(env)
+
+    resp = Rack::Response.new
+
+    hr = Time.new
+    if hr.hour >= 12
+      resp.write "#{hr} Good Afternoon!"
+    else
+      resp.writing "Good Morning!"
+    end
+    resp.finish
+  end
+
+end
