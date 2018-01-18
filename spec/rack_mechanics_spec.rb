e9f092d82e8ffd1e90d80929398068b70bc7d959
David Kuruvilla <dapawn@gmail.com>
2018-01-18 22:15:31 +0000

    automatically backed up by learn

--- spec/rack_mechanics_spec.rb
+++ spec/rack_mechanics_spec.rb
@@ -3,7 +3,7 @@ require_relative './spec_helper'
 describe "App" do
 
   it 'wishes user a good morning in the morning' do
-    allow(Time).to receive(:now).and_return(Time.utc([26, 10, 10, 2, 6, 2008, 1, 154, false, "MST"]))
+    allow(Time).to receive(:now).and_return(Time.utc(*[26, 10, 10, 2, 6, 2008, 1, 154, false, "MST"]))
   #  allow(Time).to receive(:now){
   #    Time.new(2015,11,27,9,30)
   #  }
